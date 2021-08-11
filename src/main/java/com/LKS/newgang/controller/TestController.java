6d70ed8498f04f9f1f935429c187fa311562f168
신현호 <47271188+hyeonho9877@users.noreply.github.com>
2021-08-11 21:03:02 +0900

    environment setting

--- src/main/java/com/LKS/newgang/controller/TestController.java
+++ src/main/java/com/LKS/newgang/controller/TestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
     @GetMapping("/home")
     public String getHome(){
         return "Hello World!";
