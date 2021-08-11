3beed91f3f56315bd5f1559b74b81e39a97da741
신현호 <47271188+hyeonho9877@users.noreply.github.com>
2021-08-11 20:51:03 +0900

    environment setting

---
+++ src/main/java/com/LKS/newgang/NewgangApplication.java
@@ -0,0 +1,13 @@
+package com.LKS.newgang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewgangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewgangApplication.class, args);
+	}
+
+}
