d19e5a379bc45d6406f24a1d7d2a1f5bea3a9c76
joyfulviper <79970349+joyfulviper@users.noreply.github.com>
2021-08-17 21:07:58 +0900

    getter create

--- src/main/java/com/LKS/newgang/domain/Enrolment.java
+++ src/main/java/com/LKS/newgang/domain/Enrolment.java
@@ -1,7 +1,27 @@
 package com.LKS.newgang.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Enrolment {
+    @Id
+    private int no;
+    @JoinColumn(name = "std_no")
+    @ManyToMany
+    private Student std_no;
+    @JoinColumn
+    @ManyToMany
+    private Lecture lec_no;
+
+    public int getNo() {
+        return no;
+    }
+
+    public Student getStd_no() {
+        return std_no;
+    }
+
+    public Lecture getLec_no() {
+        return lec_no;
+    }
 }
--- src/main/java/com/LKS/newgang/domain/Student.java
+++ src/main/java/com/LKS/newgang/domain/Student.java
@@ -1,7 +1,60 @@
 package com.LKS.newgang.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class Student {
+    @Id
+    private int no;
+    private String password;
+    private String greatStudent;
+    private int grade;
+    private int semester;
+    @JoinColumn(name = "campus")
+    @ManyToOne
+    private Campus campus;
+    @JoinColumn(name = "colleague")
+    @ManyToOne
+    private Colleague colleague;
+    @JoinColumn(name = "department")
+    @ManyToOne
+    private Department department;
+    private String name;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getGreatStudent() {
+        return greatStudent;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public Campus getCampus() {
+        return campus;
+    }
+
+    public Colleague getColleague() {
+        return colleague;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getNo() {
+        return no;
+    }
 }
