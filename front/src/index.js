f3fc76153de8ee3b9b356508a2c75059e5676154
ldongyeop2 <ldongyeop2@gmail.com>
2021-08-22 18:16:27 +0900

    change defalut

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App';
-import reportWebVitals from './reportWebVitals';
+import Root from './client/Root';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App/>
+        <Root />
     </React.StrictMode>,
     document.getElementById('root')
 );
-
