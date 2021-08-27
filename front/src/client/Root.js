6a2749da3ed4223155f7e9e8278ac16d6c992002
ldongyeop2 <ldongyeop2@gmail.com>
2021-08-27 17:56:25 +0900

    change defalut

--- front/src/client/Root.js
+++ front/src/client/Root.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../shared/App.css';
+import App from '../shared/App';
 
 function Root() {
     return(
